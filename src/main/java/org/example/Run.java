99427cf5da7f2ce06730058d8fadb39db3b1a3cf
Lucagiorgi95 <lucagiorgi995@gmail.com>
2023-02-07 21:32:06 +0100

    Snake -- Ver. 0.10
    "Creazione finestra di gioco"

---
+++ src/main/java/org/example/Run.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * @author LucaGiorgi
+ */
+public class Run {
+
+    public Run(){
+        FinestraDiGioco fg = new FinestraDiGioco();
+    }
+    public static void main(String[] args) {
+
+        FinestraDiGioco ws = new FinestraDiGioco();
+        ws.createWindows();
+
+
+
+
+
+
+    }
+}
